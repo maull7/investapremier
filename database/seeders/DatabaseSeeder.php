15dfa71ce2f6495dcce76e692af5705e6f1d194a
maull7 <rmgaming601@gmail.com>
2026-05-19 20:32:19 +0700

    tes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->call(AiPromptSeeder::class);
+
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
