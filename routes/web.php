331e3626e85853cff3a63106a4695ba599c2102d
maull7 <rmgaming601@gmail.com>
2026-06-11 16:14:12 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -181,7 +181,7 @@ Route::middleware(['auth', 'verified', 'role:admin,sub_admin', 'admin.permission
     Route::get('analisa-rd/lookup-sukuk-return', [AdminAnalisaRdController::class, 'lookupSukukReturn'])->name('analisa-rd.lookup-sukuk-return');
     Route::get('analisa-rd/lookup-bank-data', [AdminAnalisaRdController::class, 'lookupBankData'])->name('analisa-rd.lookup-bank-data');
 
-    // Daftar & Analisa Saham
+    // Daftar & Analisa Saham && &&
     Route::resource('saham', StockController::class)->except(['show']);
     Route::post('saham/extraction-batches', [ExtractionBatchController::class, 'storeStock'])->name('saham.extraction-batches.store');
     Route::post('saham/extraction-batches/{extractionBatch}/retry', [ExtractionBatchController::class, 'retry'])->name('saham.extraction-batches.retry');
